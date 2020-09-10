6749ed0c53b6e2dad3d99cc28e7500e469c35318
nosorrow <plamenorama@gmail.com>
2020-09-10 15:21:03 +0300

    2020-09-10

--- Crawler/getExcelData.php
+++ Crawler/getExcelData.php
@@ -1,1 +1,37 @@
 <?php
+
+include "../../PhpSpreadsheet/vendor/autoload.php";
+
+$file = "../covid-19.xlsx";
+
+$spreadsheet = PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+
+$sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+//print_r($sheetData);
+
+$titles = array_map(function ($e) {
+    return lcfirst($e);
+
+}, reset($sheetData));
+
+$data = end($sheetData);
+
+
+$date = new DateTime(array_shift($data));
+
+$date = $date->add(new DateInterval('PT10H'));
+
+$data = array_map(function ($e){
+    return (int)$e;
+}, $data);
+
+$data['A'] = $date->format("c");
+ksort($data);
+
+$toJson = ["today"=>array_combine($titles, $data)];
+
+$json = json_encode($toJson, 256);
+
+$dataToExport = trim(var_export($json, true), "'");
+
+file_put_contents('../data/data-covid.json', $dataToExport);
