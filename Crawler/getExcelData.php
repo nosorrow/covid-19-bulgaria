5be967ebcb8c8309f5dfc14dd3103e8b07078baa
nosorrow <plamenorama@gmail.com>
2020-11-02 18:32:39 +0200

    2020-11-02

--- Crawler/getExcelData.php
+++ Crawler/getExcelData.php
@@ -1,38 +1,40 @@
 <?php
 
 include "../../PhpSpreadsheet/vendor/autoload.php";
+function readExelData()
+{
+    $file = "../covid-19.xlsx";
 
-$file = "../covid-19.xlsx";
+    $spreadsheet = PhpOffice\PhpSpreadsheet\IOFactory::load($file);
 
-$spreadsheet = PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+    $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
 
-$sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+    $titles = array_map(function ($e) {
+        return lcfirst($e);
 
-$titles = array_map(function ($e) {
-    return lcfirst($e);
+    }, reset($sheetData));
 
-}, reset($sheetData));
-
-$data = end($sheetData);
+    $data = end($sheetData);
 
 
-$date = new DateTime(array_shift($data));
+    $date = new DateTime(array_shift($data));
 
-$date = $date->add(new DateInterval('PT10H'));
+    $date = $date->add(new DateInterval('PT10H'));
 
-$data = array_map(function ($e){
-    return (int)$e;
-}, $data);
+    $data = array_map(function ($e) {
+        return (float)$e;
+    }, $data);
 
-$data['A'] = $date->format("c");
-ksort($data);
+    $data['A'] = $date->format("c");
+    ksort($data);
 
-$toJson = ["today"=>array_combine($titles, $data)];
+    $toJson = ["today" => array_combine($titles, $data)];
 
-$json = json_encode($toJson, 256);
+    $json = json_encode($toJson, 256);
 
-$dataToExport = trim(var_export($json, true), "'");
+    $dataToExport = trim(var_export($json, true), "'");
 
-file_put_contents('../data/data-covid.json', $dataToExport);
+    file_put_contents('../data/data-covid.json', $dataToExport);
 
-print_r($json);
+    print_r($json);
+}
--- Crawler/mz.php
+++ Crawler/mz.php
@@ -6,145 +6,156 @@ use Symfony\Component\DomCrawler\Crawler;
 use Manufacture\ManufactureCurl;
 use Manufacture\DomCrowler;
 
-function getCOVI19($url)
-{
-    try {
-        $request = new ManufactureCurl($url);
-        $response = $request->getResponse(false);
+//$uris = parseMzLinks('https://www.mh.government.bg/bg/novini/aktualno/');
+//var_dump($uris);die;
 
-    } catch (Exception $e) {
-        echo $e->getMessage();
+//=====================================
+class Mz
+{
+
+    private $url;
+
+    public function __construct($url)
+    {
+
+        $this->url = $url;
     }
 
-    $parse = new DomCrowler($response);
-    $parse->setXpath('//*[@id="top"]/div/div/div[1]/div');
-    $elements = $parse->getContext();
+    public function getCOVI19($url)
+    {
+        try {
+            $request = new ManufactureCurl($url);
+            $response = $request->getResponse(false);
 
-    $mz = function () use ($elements) {
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
 
-        if ($elements !== null) {
-            foreach ($elements as $element) {
-                $nodes = $element->childNodes;
-                foreach ($nodes as $node) {
-                    //echo $node->nodeValue. "\n";
-                    // Броят на починалите е+[^.!?]*[.!?]
-                    preg_match("~(?:както следва:)([\S\s]+)(?:\.[^.]?)$~Um",
-                        $node->nodeValue, $match);
-                    if ($match) {
-                        return $match[1];
+        $parse = new DomCrowler($response);
+        $parse->setXpath('//*[@id="top"]/div/div/div[1]/div');
+        $elements = $parse->getContext();
+
+        $mz = function () use ($elements) {
+
+            if ($elements !== null) {
+                foreach ($elements as $element) {
+                    $nodes = $element->childNodes;
+                    foreach ($nodes as $node) {
+                        //echo $node->nodeValue. "\n";
+                        // Броят на починалите е+[^.!?]*[.!?]
+                        preg_match("~(?:както следва:)([\S\s]+)(?:\.[^.]?)$~Um",
+                            $node->nodeValue, $match);
+                        if ($match) {
+                            return $match[1];
+                        }
                     }
                 }
             }
-        }
-        return false;
+            return false;
 
-    };
+        };
 
-    if ($mz()) {
-        $stat = explode(';', $mz());
-        $stats = array_map(function ($item) {
-            return trim($item);
-        }, $stat);
-
-        // $dd = new Crawler($response);
-        // $date = $dd->filter('.newsdate li time')->text();
-        $parse->setXpath('//*[@id="top"]/div/div/div[1]/ul[1]/li/time');
-        $dd = $parse->getContext();
-        foreach ($dd as $d) {
-            $date = $d->textContent;
-        }
-        //   return json_encode([$date => $stats], JSON_UNESCAPED_UNICODE);
-
-        return $stats;
-    }
-
-    return false;
-}
-
-function parseMzLinks($url)
-{
-    try {
-        $request = new ManufactureCurl($url);
-        $response = $request->getResponse(false);
-
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
-
-    $crawler = new Crawler($response, null, $url);
-
-    return $crawler->filter('.news-list li h2 a')->each(function (Crawler $node, $i) {
-        $link = $node->link();
-        return $link->getUri();
-    });
-
-}
-
-function normalizeParsedData($data)
-{
-    //the last in line in not Ok - 23 => 'Шумен – 4, Ямбол – 6'
-    /*    $normalizedData = [];
-        $pop = array_pop($data);
-        $pop = explode(",", $pop);
-        array_push($data, ...$pop);*/
-    $data = normalizeAnomaly($data);
-    foreach ($data as $value) {
-        $dash = "-";//html_entity_decode('&ndash;');
-
-        $key = trim(strstr($value, $dash, true));
-        $city = (int)ltrim(substr(strstr($value, $dash), 0), $dash);
-        $normalizedData[$key] = $city;
-    }
-    return $normalizedData;
-}
-
-function normalizeAnomaly($arr)
-{
-    $anomaly = [];
-    $normlized = $arr;
-    foreach ($arr as $key => $value) {
-        if (strpos($value, ',') !== false) {
-            $value = explode(',', $value);
-            $value = array_map(function ($item) {
+        if ($mz()) {
+            $stat = explode(';', $mz());
+            $stats = array_map(function ($item) {
                 return trim($item);
-            }, $value);
-            array_push($anomaly, ...$value);
-            unset($normlized[$key]);
+            }, $stat);
+
+            // $dd = new Crawler($response);
+            // $date = $dd->filter('.newsdate li time')->text();
+            $parse->setXpath('//*[@id="top"]/div/div/div[1]/ul[1]/li/time');
+            $dd = $parse->getContext();
+            foreach ($dd as $d) {
+                $date = $d->textContent;
+            }
+            //   return json_encode([$date => $stats], JSON_UNESCAPED_UNICODE);
+
+            return $stats;
         }
-    }
-    $normlized = (array_merge(array_values($normlized), $anomaly));
 
-    return $normlized;
-}
-
-//===================================================================================
-
-$uris = parseMzLinks('https://www.mh.government.bg/bg/novini/aktualno/');
-//var_dump($uris);die;
-
-
-function printCovid19($url){
-
-    $parsedRaWCitiesNew = getCOVI19($url);
-
-    $cities = [];
-    if ($parsedRaWCitiesNew !== false) {
-        foreach ($parsedRaWCitiesNew as $valuecity) {
-            $cities[]['name'] = $valuecity;
-        }
-    } else {
-        trigger_error("TRIGGER ERROR: Nothing to show! Possible wrong URL!", E_USER_ERROR);
+        return false;
     }
 
-    $json = (json_encode(['cities'=>$cities], 256));
+    protected function parseMzLinks($url)
+    {
+        try {
+            $request = new ManufactureCurl($url);
+            $response = $request->getResponse(false);
 
-    $data =  trim(var_export($json, true), "'");
-    file_put_contents('../data/mun.json', $data);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
 
-    print_r($data);
+        $crawler = new Crawler($response, null, $url);
 
+        return $crawler->filter('.news-list li h2 a')->each(function (Crawler $node, $i) {
+            $link = $node->link();
+            return $link->getUri();
+        });
+
+    }
+
+    protected function normalizeParsedData($data)
+    {
+        //the last in line in not Ok - 23 => 'Шумен – 4, Ямбол – 6'
+        /*    $normalizedData = [];
+            $pop = array_pop($data);
+            $pop = explode(",", $pop);
+            array_push($data, ...$pop);*/
+        $data = $this->normalizeAnomaly($data);
+        foreach ($data as $value) {
+            $dash = "-";//html_entity_decode('&ndash;');
+
+            $key = trim(strstr($value, $dash, true));
+            $city = (int)ltrim(substr(strstr($value, $dash), 0), $dash);
+            $normalizedData[$key] = $city;
+        }
+        return $normalizedData;
+    }
+
+    protected function normalizeAnomaly($arr)
+    {
+        $anomaly = [];
+        $normlized = $arr;
+        foreach ($arr as $key => $value) {
+            if (strpos($value, ',') !== false) {
+                $value = explode(',', $value);
+                $value = array_map(function ($item) {
+                    return trim($item);
+                }, $value);
+                array_push($anomaly, ...$value);
+                unset($normlized[$key]);
+            }
+        }
+        $normlized = (array_merge(array_values($normlized), $anomaly));
+
+        return $normlized;
+    }
+
+    public function getUrls()
+    {
+        return $this->parseMzLinks($this->url);
+    }
+
+    public function printCovid19($url){
+
+        $parsedRaWCitiesNew = $this->getCOVI19($url);
+
+        $cities = [];
+        if ($parsedRaWCitiesNew !== false) {
+            foreach ($parsedRaWCitiesNew as $valuecity) {
+                $cities[]['name'] = $valuecity;
+            }
+        } else {
+            trigger_error("TRIGGER ERROR: Nothing to show! Possible wrong URL!", E_USER_ERROR);
+        }
+
+        $json = (json_encode(['cities'=>$cities], 256));
+
+        $data =  trim(var_export($json, true), "'");
+        file_put_contents('../data/mun.json', $data);
+
+        print_r($data);
+
+    }
 }
-
-//==================================
-
-printCovid19($uris[0]);
