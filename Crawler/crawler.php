660fe0b5d7b8ee04f7dfadcc387ad0f62fa00a22
nosorrow <plamenorama@gmail.com>
2020-12-04 14:46:57 +0200

    2020-12-04

--- Crawler/crawler.php
+++ Crawler/crawler.php
@@ -4,6 +4,6 @@ include_once 'getExcelData.php';
 
 $mz = new Mz('https://www.mh.government.bg/bg/novini/aktualno/');
 $url = $mz->getUrls();
-$mz->printCovid19($url[0]);
+$mz->printCovid19($url[2]);
 echo "\n\n";
 readExelData();
